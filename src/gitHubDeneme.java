f3c1018b41f462af423002a0458001444337289c
Hasan ALAGOZ <123511537+alagozhasan@users.noreply.github.com>
2023-02-22 18:45:29 +0300

    Merhaba Hocam çözdüm

--- src/gitHubDeneme.java
+++ src/gitHubDeneme.java
@@ -2,6 +2,6 @@ public class gitHubDeneme {
     public static void main(String[] args) {
         System.out.println(" Merhaba ");
         System.out.println(" Merhaba arkadaşlar ");
-
+        System.out.println("Hocam ilginç bir şekilde sizin classlarınız standart yolda gözükmedi");
     }
 }
