5d318e0ad24316e19193cfeb2f400799e58d5e3c
bilalMavus <bilalmavuss@gmail.com>
2023-02-22 18:33:47 +0300

    Merhaba arkadaşlar mesaj satırını oluşturdum

--- src/gitHubDeneme.java
+++ src/gitHubDeneme.java
@@ -1,6 +1,7 @@
 public class gitHubDeneme {
     public static void main(String[] args) {
         System.out.println(" Merhaba ");
-        
+        System.out.println(" Merhaba arkadaşlar ");
+
     }
 }
